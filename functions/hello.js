0cd650dcce677014cf900d676ab8c4ee8ab8849e
weixiaohu <weixiaohu@agilesc.com>
2018-12-12 15:50:04 +0800

    修改接口返回值

--- functions/hello.js
+++ functions/hello.js
@@ -5,6 +5,6 @@ exports.handler = function (event, context, callback) {
     };
     callback(null, {
         statusCode: 200,
-        body: JSON.stringify(data)
+        body: JSON.stringify([data])
     });
 }
