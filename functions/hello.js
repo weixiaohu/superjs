23f9e909b1db23e144c60d1dad711557fe093fd2
weixiaohu <weixiaohu@agilesc.com>
2018-12-12 15:37:17 +0800

    INIT

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,10 @@
 exports.handler = function (event, context, callback) {
+    let data = {
+        "terminalUrl": "https://datav.aliyun.com/share/60a862488f8563e284c126989aabf8e9",
+        "dealerUrl": "https://datav.aliyun.com/share/bca4f8ee9a2abcd1f208719ce40cb070"
+    };
     callback(null, {
         statusCode: 200,
-        body: "world"
+        body: JSON.stringify(data)
     });
 }
