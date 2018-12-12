201bdf26332f77cdb7becf7db0e5b94f1b9d260a
weixiaohu <weixiaohu@agilesc.com>
2018-12-12 18:14:36 +0800

    修改接口返回值

--- functions/hello.js
+++ functions/hello.js
@@ -1,10 +1,14 @@
 import readFilePromise from "fs-readfile-promise"
+const path = require('path');
 
 exports.handler = async  (event, context, callback) => {
-    let type = event.queryStringParameters.type
-    let jsonData = await readFilePromise(`./json/${type}.json`,"utf8");
+    const dirname = __dirname;
+    const currpath = path.dirname(__filename)
+    const filename = __filename;
+    let type = event.queryStringParameters.type;
+    //let jsonData = await readFilePromise(`./json/${type}.json`,"utf8");
     callback(null, {
         statusCode: 200,
-        body: jsonData
+        body: `${dirname} -- ${currpath} -- ${filename}`
     });
 }
